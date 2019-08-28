be4cc1db7d860a517adcfa8fa67ef5c8fb4ca651
Nils Sutter <nilssutter@gmx.de>
2019-08-28 14:53:59 +0200

    remove 'require rubygems'

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
-require 'rubygems'
 require "sinatra"
 require 'sinatra/activerecord'
 
 class App < Sinatra::Base
-  #egister Sinatra::ActiveRecordExtension
-
   get "/" do
     "Hello there"
   end
